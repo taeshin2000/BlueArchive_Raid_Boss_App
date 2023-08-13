90c9d31ad09f85c53d64cf6358c5a154b3711c45
taeshin2000 <68556214+taeshin2000@users.noreply.github.com>
2023-08-13 17:08:16 +0700

    fix deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Boss from "./Pages/Boss";
-import BossDetail from "./Pages/BossDetail";
-import About from "./Pages/About";
+import Boss from "./routes/Boss";
+import BossDetail from "./routes/BossDetail";
+import About from "./routes/About";
 
 import "./App.css";
 
